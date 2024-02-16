c9e768b7a5ece0d0ea63c270ae9216b5b8a2ef3c
sheaal <alisandra.sh05@gmail.com>
2024-02-16 19:50:05 +0700

    задание3 комит2

---
+++ js/main.js
@@ -0,0 +1,75 @@
+const app = new Vue({
+    el: '#app',
+    data() {
+        return {
+            columns: [
+                {
+                    title: 'Запланированные задачи',
+                    tasks: [
+                        {
+                            id: 1,
+                            title: '',
+                            description: '',
+                            deadline: '',
+                            createdAt: new Date(),
+                            updatedAt: null,
+                        },
+                    ],
+                },
+                {
+                    title: 'Задачи в работе',
+                    tasks: [],
+                },
+                {
+                    title: 'Тестирование',
+                    tasks: [],
+                },
+                {
+                    title: 'Выполненные задачи',
+                    tasks: [],
+                },
+            ],
+        };
+    },
+    methods: {
+        addTask(column) {
+            const newTask = {
+                id: Date.now(),
+                title: '',
+                description: '',
+                deadline: '',
+                createdAt: new Date(),
+                updatedAt: null,
+            };
+            column.tasks.push(newTask);
+            this.$nextTick(() => {
+                this.isFirstTask(column.tasks.length - 1, column.tasks);
+            });
+        },
+        moveTask(task, targetColumn) {
+            const sourceColumnIndex = this.columns.findIndex((column) => column.tasks.includes(task));
+            this.columns[sourceColumnIndex].tasks = this.columns[sourceColumnIndex].tasks.filter((t) => t.id !== task.id);
+            targetColumn.tasks.push(task);
+            this.updateTaskDates(task);
+        },
+        removeTask(task, column) {
+            column.tasks = column.tasks.filter((t) => t.id !== task.id);
+            this.updateTaskDates(task);
+        },
+        saveTask(task) {
+            task.updatedAt = new Date();
+        },
+        isEditingTask(task) {
+            return task.updatedAt === null;
+        },
+        isFirstTask(taskIndex, tasks) {
+            return taskIndex === 0 && tasks[taskIndex].updatedAt === null;
+        },
+        updateTaskDates(task) {
+            if (task.updatedAt === null) {
+                task.createdAt = new Date();
+            }
+            task.updatedAt = new Date();
+        },
+    },
+});
